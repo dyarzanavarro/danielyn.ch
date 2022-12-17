cf5700b3d5693d32880e48e842f2e42bea448c6d
Dan <danielyarza1@gmail.com>
2022-12-17 20:18:34 +0100

    movement on click

--- pages/village.vue
+++ pages/village.vue
@@ -16,64 +16,59 @@ export default {
     document.getElementById("pixi").appendChild(app.view);
     //w = 37
     //h =46
-    const container = new PIXI.Container();
-    app.stage.addChild(container);
-    // Create a new texture
-    const sprite = PIXI.Sprite.from("/img/pixiImages/background.png");
-    const womanWalk = [
-      "/img/pixiImages/woman-walk-1.png",
-      "/img/pixiImages/woman-walk-2.png",
-      "/img/pixiImages/woman-walk-3.png",
-      "/img/pixiImages/woman-walk-4.png",
-      "/img/pixiImages/woman-walk-5.png",
-      "/img/pixiImages/woman-walk-6.png",
-    ];
-    const character = PIXI.AnimatedSprite.fromFrames(womanWalk);
-    character.animationSpeed = 1 / 20; // 6 fps
-    character.play();
-    sprite.height = 1080;
-    sprite.width = 1920;
-    let playerSheet;
-    // Set the position and dimensions of the sprite
-    character.anchor.set(1);
-    character.x = app.view.height / 3;
-    character.y = app.view.height / 2;
-    character.width = 64;
-    character.height = 64;
-    // Enable interactivity for the character sprite
-    app.stage.interactive = true;
-    window.addEventListener("keydown", keyDown);
-    window.addEventListener("keyup", keyUp);
-    let keys = {};
-    let keysDiv;
-    keysDiv = document.querySelector("#keys");
-    app.ticker.add(gameLoop);
-    function keyDown(e) {
-      keys[e.keyCode] = true;
+    const titleScreen = new PIXI.Container();
+    const titleText = new PIXI.Text("Welcome", {
+      fontSize: 60,
+      fill: 0xffffff,
+    });
+    titleScreen.addChild(titleText);
+    titleText.x = app.view.width / 4;
+    titleText.y = app.view.height / 4;
+
+    app.stage.addChild(titleScreen);
+
+    titleScreen.interactive = true;
+    titleScreen.on("pointertap", startGame);
+
+    function startGame() {
+      // Remove the title screen and start the game
+      app.stage.removeChild(titleScreen);
+      // Start game logic here
+      // Create a new texture
+      const sprite = PIXI.Sprite.from("/img/pixiImages/background.png");
+      const womanWalk = [
+        "/img/pixiImages/woman-walk-1.png",
+        "/img/pixiImages/woman-walk-2.png",
+        "/img/pixiImages/woman-walk-3.png",
+        "/img/pixiImages/woman-walk-4.png",
+        "/img/pixiImages/woman-walk-5.png",
+        "/img/pixiImages/woman-walk-6.png",
+      ];
+      const character = PIXI.AnimatedSprite.fromFrames(womanWalk);
+      character.animationSpeed = 1 / 20; // 6 fps
+      character.play();
+      sprite.height = 1080;
+      sprite.width = 1920;
+      let playerSheet;
+      // Set the position and dimensions of the sprite
+      character.anchor.set(1);
+      character.x = app.view.height / 3;
+      character.y = app.view.height / 2;
+      character.width = 64;
+      character.height = 64;
+      character.interactive = true;
+      // Enable interactivity for the character sprite
+      app.stage.interactive = true;
+
+      app.stage.on("pointertap", onCanvasClick);
+
+      function onCanvasClick(event) {
+        character.x = event.data.global.x;
+        // Move the character to the clicked position
+      }
+      // Add the application to the page
+      app.stage.addChild(sprite, character);
     }
-    function keyUp(e) {
-      keys[e.keyCode] = false;
-    }
-    function gameLoop() {
-      //A
-      if (keys["65"]) {
-        character.x -= 7;
-      }
-      //D
-      if (keys["68"]) {
-        character.x += 7;
-      }
-      //W
-      if (keys["87"]) {
-        character.y -= 7;
-      }
-      //S
-      if (keys["83"]) {
-        character.y += 7;
-      }
-    }
-    // Add the application to the page
-    app.stage.addChild(sprite, character);
   },
 };
 </script>
